9f75a4f38b56214344def3259f083466272cb57f
limi6n <6nlimitless@gmail.com>
2021-01-10 16:55:16 +0900

    bug fix

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,9 @@
+from _testcapi import get_kwargs
+
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -22,8 +24,10 @@ class ProfileCreateView(CreateView):
         temp_profile = form.save(commit=False) #commit은 하지 않고 임시데이터가 됨
         temp_profile.user = self.request.user #user를 제외한 사진, 닉네임, 메시지 정보는 있으므로 로그인 되어있는 user 정보를 넣어준다
         temp_profile.save()
-
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', get_kwargs={'pk': self.object.user.pk})
 
 
 @method_decorator(profile_ownership_required, 'get')
