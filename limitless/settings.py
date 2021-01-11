7f46b483639d6f42e39d0af533a03867fda183b8
limi6n <6nlimitless@gmail.com>
2021-01-11 22:25:22 +0900

    projectapp init

--- limitless/settings.py
+++ limitless/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
 ]
 
 MIDDLEWARE = [
